eb1755952a20f6e71d29596f0d91b555d8089ca2
Ankit Shaji <ankitshaji@gmail.com>
2023-06-02 07:17:25 +1200

    adding clean-webpack-plugin plugin to clear the output dist directory

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,12 +3,16 @@
 const path = require("path"); //pathLiteralJsObject //built-in serverside js pathWebApi library/non global core module
 const common = require("./webpack.common"); //commonLiteralJsObject //user-created serverside js webpack.commonWebApi library - js module file
 const { merge } = require("webpack-merge"); //functionObject //thirdparty serverside js webpack-mergeWebApi library/module/npm package
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); //Class //thirdparty serverside js clean-webpack-pluginWebApi library/module/npm package
 
 module.exports = merge(common, {
-  //minify [name].[contenthash].bundle.js script file (ie dont remove the default terser-webpack-plugin in optimization->minimizer)
+  //minify [name].[contenthash].bundle.js script file (ie dont remove the default terser-webpack-plugin plugin in optimization->minimizer)
   mode: "production",
+  plugins: [new CleanWebpackPlugin()],
   output: {
     filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    //new alternative instead of using clean-webpack-plugin plugin
+    //clean: true,
   },
 });
