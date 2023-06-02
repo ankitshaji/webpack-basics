f46c84da293d46db3179eb13c4c52830e1597363
Ankit Shaji <ankitshaji@gmail.com>
2023-06-03 03:30:04 +1200

    adding html-webpack-plugin plugin to create index.html that auto links

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,9 +3,10 @@
 const path = require("path"); //pathLiteralJsObject //built-in serverside js pathWebApi library/non global core module
 const common = require("./webpack.common"); //commonLiteralJsObject //user-created serverside js webpack.commonWebApi library - js module file
 const { merge } = require("webpack-merge"); //functionObject //thirdparty serverside js webpack-mergeWebApi library/module/npm package
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //Class //thirdparty serverside js html-webpack-pluginWebApi library/module/npm package
 
 module.exports = merge(common, {
-  //dont minify [name].bundle.js script file (ie remove the default terser-webpack-plugin in optimization->minimizer)
+  //dont minify [name].bundle.js script file (ie remove the default terser-webpack-plugin pugin in optimization->minimizer)
   mode: "development",
   //remove "eval" devtool (ie. eval())
   //devtool: false,
@@ -13,4 +14,5 @@ module.exports = merge(common, {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
 });
